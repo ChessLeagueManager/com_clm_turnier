0ea98ecdb220bb88704b64281d1b17a403c3d4df
ahrubesch <andreas.hrubesch@arcor.de>
2018-07-11 17:53:09 +0200

    Dokumentation

--- site/helpers/html/thead.php
+++ site/helpers/html/thead.php
@@ -8,40 +8,42 @@
  */
 
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 /**
- * Utility class for THead JavaScript behaviors 
- * floatThead
- * 
+ * Utility class für "jquery.floatThead" JavaScript Bibliothek
+ *
  * @since 2.1
  * @see https://github.com/mkoryak/floatThead
+ * @see http://mkoryak.github.io/floatThead/
  */
 abstract class JHtmlThead {
-
-    /**
-     * @var    array  Array containing information for loaded files
-     */
-    protected static $loaded = array();
-    
-    /**
-     * Method to load the jQuery JavaScript framework into the document head
-     */
-    public static function framework() {
-        // Only load once
-        if (!empty(static::$loaded[__METHOD__])) {
-            return;
-        }
-
-        // load jQuery JavaScript framework
-        JHtml::_('jquery.framework');
-        
-        // load floatThead JavaScript framework
-        JHtml::_('script', 'https://cdnjs.cloudflare.com/ajax/libs/floatthead/2.1.2/jquery.floatThead.js');
-
-        //
-        $document = JFactory::getDocument();
-        $document->addScriptDeclaration('
+	
+	/**
+	 *
+	 * @var array Array containing information for loaded files
+	 */
+	protected static $loaded = array ();
+	
+	/**
+	 * läd die JavaScript Frameworks für schwebenden Tabellenüberschriften in
+	 * den HTML Dokumentkopf.
+	 */
+	public static function framework() {
+		// Only load once
+		if (! empty ( static::$loaded [__METHOD__] )) {
+			return;
+		}
+		
+		// load jQuery JavaScript framework
+		JHtml::_ ( 'jquery.framework' );
+		
+		// load floatThead JavaScript framework
+		JHtml::_ ( 'script', 'https://cdnjs.cloudflare.com/ajax/libs/floatthead/2.1.2/jquery.floatThead.js' );
+		
+		//
+		$document = JFactory::getDocument ();
+		$document->addScriptDeclaration ( '
             if (typeof jQuery == "function") {
                 jQuery(document).ready(function() {        
                     jQuery(".theadFloatingHeader").floatThead({position: "absolute"});
@@ -51,21 +53,25 @@ abstract class JHtmlThead {
                     $(".theadFloatingHeader").floatThead({position: "absolute"});
                 });
             }
-        ');
-        
-        static::$loaded[__METHOD__] = true;
-        
-        return;
-    }
-    
-    /**
-     * 
-     * @param boolean $active
-     */
-    public static function tableClass($active = true) {
-        if ($active == true)
-            echo 'class="theadFloatingHeader"';
-    }
+        ' );
+		
+		static::$loaded [__METHOD__] = true;
+		
+		return;
+	}
+	
+	/**
+	 * erstellt das Table Class Element
+	 *
+	 * @param boolean $active
+	 *        	true = schwebenden Tabellenüberschriften aktiv
+	 *        	
+	 */
+	public static function tableClass($active = true) {
+		if ($active == true) {
+			echo 'class="theadFloatingHeader"';
+		}
+	}
 }
 
 ?>
